f0862928f970f768397ba39755a66fd3a0ea7b76
JesusALO <a6519150014@utch.edu.mx>
2021-08-11 23:03:02 -0600

    changes

--- api/settings.py
+++ api/settings.py
@@ -28,9 +28,12 @@ LOCAL = False
 
 ALLOWED_HOSTS = ['*']
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER ='jl058227@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = 'kcfnboandzavllps'
+EMAIL_USE_TLS = True
 
 # Application definition
 
